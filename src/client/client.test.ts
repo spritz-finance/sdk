b3239a5a1dbd9be7240c474eb22a4b557e34960b
Eliyahu Gusovsky <eliyahud@gmail.com>
2022-09-16 15:15:39 +0300

    clean

--- src/client/client.test.ts
+++ src/client/client.test.ts
@@ -2,6 +2,11 @@ import { SpritzPaySDK } from './index'
 import { Network } from '../networks'
 import { ethers } from 'ethers'
 
+import { config as dotenvConfig } from 'dotenv'
+import { resolve } from 'path'
+
+dotenvConfig({ path: resolve(__dirname, '../.env') })
+
 const WBTC_POLYGON_ADDRESS = '0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6'
 const AAVE_POLYGON_ADDRESS = '0xD6DF932A45C0f255f85145f286eA0b292B21C90B'
 
@@ -9,7 +14,7 @@ describe('SpritzPaySDK', () => {
   const sdk = new SpritzPaySDK({
     network: Network.Polygon,
     provider: new ethers.providers.JsonRpcProvider(
-      `https://polygon-mainnet.g.alchemy.com/v2/gl3dK8sSOYbnYkzH-DGSjWG46dCVwCJ5`,
+      `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_POLYGON_MAINNET_KEY}`,
       137,
     ),
     staging: true,
