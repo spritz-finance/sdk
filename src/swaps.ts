ca07e785f469f11e0221e47dc8ea6e2062ef3e1f
Laurence Davies <ljd.davies@gmail.com>
2022-12-01 11:19:14 +0200

    eth v2

--- src/swaps.ts
+++ src/swaps.ts
@@ -1,5 +1,6 @@
 import { Network } from './networks'
 
-const V3_SWAP_NETWORKS = [Network.Ethereum, Network.Arbitrum, Network.Optimism]
+// const V3_SWAP_NETWORKS = [Network.Ethereum, Network.Arbitrum, Network.Optimism]
+const V3_SWAP_NETWORKS = [Network.Arbitrum, Network.Optimism]
 
 export const isV3SwapNetwork = (network: Network) => V3_SWAP_NETWORKS.includes(network)
