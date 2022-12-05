c9cf898f398d4ecb3e319c60f4d20866104f35bd
Laurence Davies <ljd.davies@gmail.com>
2022-12-05 13:39:06 +0200

    Use v3 for mainnet

--- src/swaps.ts
+++ src/swaps.ts
@@ -1,6 +1,5 @@
 import { Network } from './networks'
 
-// const V3_SWAP_NETWORKS = [Network.Ethereum, Network.Arbitrum, Network.Optimism]
-const V3_SWAP_NETWORKS = [Network.Arbitrum, Network.Optimism]
+const V3_SWAP_NETWORKS = [Network.Ethereum, Network.Arbitrum, Network.Optimism]
 
 export const isV3SwapNetwork = (network: Network) => V3_SWAP_NETWORKS.includes(network)
